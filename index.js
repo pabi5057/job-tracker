a31bfb4e120cbdf5d94b9242ca76eac9ad88189b
pabi5057 <pabitra.panda.5057@gmail.com>
2025-05-30 18:14:34 +0530

    remove locahost origin

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    //origin:'https://job-tracker-frontend-jade.vercel.app',
-    origin:'http://localhost:5173',
+    origin:'https://job-tracker-frontend-jade.vercel.app',
+   // origin:'http://localhost:5173',
     credentials:true
 }
 
